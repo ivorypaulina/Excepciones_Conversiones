e68299693b058d21fa7c2a07a5583462345ab64d
ivorypaulina <ivorypaulina10@gmail.com>
2026-05-17 01:43:59 -0500

    Actividad2

--- src/Actividad1.java
+++ src/Actividad1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Actividad1 {
     public static void main (String[] args){
+
          Scanner sc = new Scanner(System.in);
 
          try{ //Significa "Intenta ejecutar este codigo"
---
+++ src/EdadInvalidaException.java
@@ -0,0 +1,15 @@
+public class EdadInvalidaException extends Exception {
+
+    private int edadIngresada;
+
+    public EdadInvalidaException(int edadIngresada) {
+
+        super("Edad inválida: " + edadIngresada);
+
+        this.edadIngresada = edadIngresada;
+    }
+
+    public int getEdadIngresada() {
+        return edadIngresada;
+    }
+}
